c5157c81684da0c664cc8d2c71b18bd4e7dce321
Andy McDonald <mothershipya@yahoo.com>
2015-01-08 09:25:21 -0500

    Start GitHub Friends

---
+++ GithubFriends/GithubFriends/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubFriends
+//
+//  Created by andrew Mcdonald on 1/8/15.
+//  Copyright (c) 2015 Andrew McDonald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
